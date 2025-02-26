cbae40ae3bc30a80e6c0bda2a21350d1d4d8fbfc
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-02-26 15:30:17 +0530

    all

--- namespace.cpp
+++ namespace.cpp
@@ -1,45 +1,82 @@
-// #include<iostream>
-// using namespace std;
-// namespace cybrom {
-//     int a =90;
-//     int b= 100;
-//     void show(){
-//         cout<<"result = "<<a+b;
-//     }
-//     class bhopal{
-//         public:int sqr(int a){
-//             return a*a;
-//         }
-//     };
-//     class iostreamx{
-//         public:void fun(){
-//             cout<<"\n fun \n";
-//         }
-//     }
-//     coutx;          // object create
-// }
-// using namespace cybrom;
-// int main(){
-//     cout<<a<<"\n";
-//     cout<<b<<"\n";
-//     show();
-//     bhopal b;
-//     cout<<"\n";
-//     cout<<b.sqr(8);
-//     coutx.fun();        
-// }
-#include<iostream>
-using namespace std;
-namespace cybrom {
+// NAMESPACE IN DSA 
+// Namespace: A way to group variables, functions, and classes under a unique name.
+// Functions: Blocks of reusable code.
+// Classes & Objects: Used to implement Object-Oriented Programming (OOP).
+// Using Namespace: Makes it easier to access elements inside a namespace.
 
-    void student({
-        cout<<"bhopal";
-    })
+#include <iostream>
+using namespace std;
+// Defining a namespace named 'cybrom'
+namespace cybrom {
+    int a = 90;  // Global variables inside the namespace
+    int b = 100;
+
+    // Function to display the sum of 'a' and 'b'
+    void show() {
+        cout << "Result = " << a + b << endl;
+    }
+
+    // Class definition inside the namespace
+    class Bhopal {
+    public:
+        int square(int num) {  // Function to return the square of a number
+            return num * num;
+        }
+    };
+
+    // Class with a function
+    class IOStreamX {
+    public:
+        void fun() {
+            cout << "\nFun function executed\n";
+        }
+    };
+}
+
+using namespace cybrom;  // Allows direct access to 'cybrom' namespace members
+
+int main() {
+    cout << a << "\n";  // Accessing variables from 'cybrom' namespace
+    cout << b << "\n";
+    
+    show();  // Calling function from namespace
+
+    Bhopal obj;  // Creating an object of class 'Bhopal'
+    cout << "\nSquare of 8 = " << obj.square(8) << endl;  // Calling the method
+
+    IOStreamX obj2;  // Creating an object of class 'IOStreamX'
+    obj2.fun();  // Calling the function
+
+    return 0;
+}
+// nested namespaces in C++
+// Namespaces: A way to organize code and prevent name conflicts.
+// Nested Namespaces: One namespace inside another.
+// Functions: Defining functions inside namespaces.
+
+#include <iostream>  // Include standard input-output library
+using namespace std; // Allow direct use of standard namespace functions like cout
+
+// Creating a namespace named 'cybrom'
+namespace cybrom {
+    // Function inside 'cybrom' namespace
+    void student() {
+        cout << "\nstudent\n";
+    }
+
+    // Nested namespace inside 'cybrom'
+    namespace bhopal {
+        // Function inside 'bhopal' namespace
+        void student() {
+            cout << "\nbhopal\n";
         }
     }
 }
-using namespace cybrom;
-int main(){
-cybrom::student();
-cybrom::bhopal::student();   
+
+int main() {
+    cybrom::student();        // Calls student() function from 'cybrom' namespace
+    cybrom::bhopal::student(); // Calls student() function from 'bhopal' namespace inside 'cybrom'
+    
+    return 0;
 }
+// Note: Nested namespaces allow better organization when dealing with large projects.
