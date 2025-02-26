c944c42a152e71182719b8a638a940cb4370d63d
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-02-26 13:37:56 +0530

    all

---
+++ namespace.cpp
@@ -0,0 +1,45 @@
+// #include<iostream>
+// using namespace std;
+// namespace cybrom {
+//     int a =90;
+//     int b= 100;
+//     void show(){
+//         cout<<"result = "<<a+b;
+//     }
+//     class bhopal{
+//         public:int sqr(int a){
+//             return a*a;
+//         }
+//     };
+//     class iostreamx{
+//         public:void fun(){
+//             cout<<"\n fun \n";
+//         }
+//     }
+//     coutx;          // object create
+// }
+// using namespace cybrom;
+// int main(){
+//     cout<<a<<"\n";
+//     cout<<b<<"\n";
+//     show();
+//     bhopal b;
+//     cout<<"\n";
+//     cout<<b.sqr(8);
+//     coutx.fun();        
+// }
+#include<iostream>
+using namespace std;
+namespace cybrom {
+
+    void student({
+        cout<<"bhopal";
+    })
+        }
+    }
+}
+using namespace cybrom;
+int main(){
+cybrom::student();
+cybrom::bhopal::student();   
+}
