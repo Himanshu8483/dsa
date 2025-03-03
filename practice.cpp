303114fb65eafd79267205cd246d03babb9e7765
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-03-03 13:40:32 +0530

    all

---
+++ practice.cpp
@@ -0,0 +1,37 @@
+// // pair 
+// #include<iostream>
+// using namespace std;
+// #include<vector>
+// int main(){
+//     // vector<int,int>p1{101,22};
+//     // p1= make_pair(102,"12");
+//     // cout<<p1.first;
+
+//     vector<pair<int, string>>v;
+//     int roll, n;
+//     string name;
+//     cout<<"enter how many records\n";
+//     cin>>n;     // 5
+//     for(int i = 1; i<=n; i++){  
+//         cout<<"Enter roll no\n" ;
+//         cin>>roll;
+//         cout<<"enter name\n :";
+//         v.push_back(make_pair(roll,name));
+//     }
+//     cout<<"Results are\n";
+//     for(int i=0; i<v.size(); i++){
+//         cout<<v[i].first<<":"<<v[i].second<<"\n";
+//     }
+// }
+
+// tuple 
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<tuple>
+int main(){
+    tuple<int, string,int> student{101, "joy", 86};
+    cout<<"roll no= "<<get<0>(student)<<"\n";
+    cout<<"name= "<<get<1>(student)<<"\n";
+    cout<<"marks= "<<get<2>(student)<<"\n";
+}
