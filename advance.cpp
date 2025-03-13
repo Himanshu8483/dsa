7d36125a2f048131ea47a2b9ad6fac3b2451d896
Himanshu Kushwaha <himanshukushwahabkp@gmail.com>
2025-03-13 17:35:10 +0530

    all

--- advance.cpp
+++ advance.cpp
@@ -1100,7 +1100,7 @@ int main() {
     // Display the list
     cout << "\nList before sorting and removing duplicates:\n";
     for (auto p : f) cout << p << "\t";
-
+    // f.pop_front();       // for delete
     // Sorting and removing duplicates
     f.sort();
     f.unique();
@@ -1170,17 +1170,6 @@ Final Takeaways
 2.	Use sort() before unique() to remove all duplicates.
 3.	Always sort lists before merge().
 4.	Prefer forward_list over list when backward traversal isn’t needed.
-
-
-
-
-
-
-
-
-
-
-
 
 
 Notes on list (Doubly Linked List) in C++ STL
@@ -1260,6 +1249,82 @@ int main() {
 
     return 0;
 }
+
+// Example by Vikash Sir 
+#include<iostream>
+#include<algorithm> // For find function
+#include<list> // Include the list header file
+using namespace std;
+
+int main(){
+    // Creating a doubly linked list using std::list
+    list<int> l1;
+
+    // Insert elements at the front of the list
+    l1.push_front(10);
+    l1.push_front(5);
+    l1.push_front(40);
+    
+    // Insert element at the back of the list
+    l1.push_back(50);
+
+    // Display the list
+    cout << "Initial list: \n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Uncomment below lines if you want to remove front and back elements
+    // l1.pop_front(); // Remove first element
+    // l1.pop_back(); // Remove last element
+    
+    cout << "\nAfter delete (if pop_front/pop_back used):\n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Insert an element at a specific position
+    auto t = l1.begin(); // Iterator pointing to the first element
+    advance(t, 2); // Move iterator to the 3rd position (0-based index)
+    l1.insert(t, 1000); // Insert 1000 at the 3rd position
+    
+    cout << "\nAfter inserting 1000 at position 3:\n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Remove an element at a specific position
+    auto t1 = l1.begin();
+    advance(t1, 3); // Move iterator to the 4th element (0-based index)
+    l1.erase(t1); // Remove the element at this position
+    
+    cout << "\nAfter removing element at position 4:\n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Update an element at a specific position
+    auto t3 = l1.begin();
+    advance(t3, 1); // Move iterator to the 2nd element (0-based index)
+    *t3 = 900; // Update the value at this position
+    
+    cout << "\nAfter updating the 2nd element to 900:\n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Searching for an element in the list
+    auto g = find(l1.begin(), l1.end(), 900); // Find 900 in the list
+    if(g != l1.end()){
+        cout << "\nElement found: " << *g;
+    }
+    else{
+        cout << "\nElement not found";
+    }
+    
+    return 0;
+}
+
 ________________________________________
 5. Comparison: list vs. forward_list
 Feature	list (Doubly Linked)	forward_list (Singly Linked)
--- practice.cpp
+++ practice.cpp
@@ -1,100 +1,73 @@
-// // Forward List (singly link list) use in Directed Graph
-// // list (doubly link list) use in Undirected Graph
-// // it has so many inbuilt function 
-// // for unique value must sort an array to remove duplicate 
-// #include <iostream>
-// using namespace std;
-// #include <forward_list>
-
-// int main(){
-//     forward_list<int>f;     // or {3,9,1,4,5};
-//     f.push_front(3);
-//     f.push_front(3);
-//     f.push_front(3);
-//     f.push_front(56);
-//     cout<<"\n list \n";
-//     for(auto p: f){
-//         cout<<p<<"\t";
-//     }
-//     // f.pop_front();       // for delete
-//     f.sort();           // for sorting
-//     f.unique();         // for unique (remove duplicate)
-// cout<<"\nafter delete \n";
-//     for(auto p: f){
-//         cout<<p<<"\t";
-//     }
-//     cout<<"\n f1 list\n";
-//     forward_list<int>f1{50,3,1,2,5};
-//     forward_list<int>f2{4,30,70,32,1,3};
-//     for(auto p: f1){
-//         cout<<p<<"\t";
-//     }
-//     cout<<"\n f2 list\n";
-//     for(auto p: f2){
-//         cout<<p<<"\t";
-//     }
-//     cout<<"\nmerge\n";
-//     f1.sort();
-//     f2.sort();
-//     f1.merge(f2);
-
-//     for(auto p: f1){
-//         cout<<p<<"\t";
-//     }
-
-//     return 0;
-// }
-
-// doubly list 
 #include<iostream>
+#include<algorithm> // For find function
+#include<list> // Include the list header file
 using namespace std;
-#include<algorithm>
-#include<list>
+
 int main(){
-    list<int>l1;
+    // Creating a doubly linked list using std::list
+    list<int> l1;
+
+    // Insert elements at the front of the list
     l1.push_front(10);
     l1.push_front(5);
     l1.push_front(40);
+    
+    // Insert element at the back of the list
     l1.push_back(50);
-    for(auto p:l1){
-        cout<<p<<"\t";
-    }
-    // l1.pop_front();
-    // l1.pop_back();
-    cout<<"\nafter delete\n";
-    for(auto p:l1){
-        cout<<p<<"\t";
-    }
-    auto t=l1.begin();
-    advance(t,2);
-    l1.insert(t,1000);
-    cout<<"\nafter advance insert at any postion\n";
-    for(auto p:l1){
-        cout<<p<<"\t";
-    }
-    auto t1 = l1.begin();
-    advance(t1, 3);
-    l1.erase(t1);
-    cout<<"\n after remove\n";
-    for(auto p:l1){
-        cout<<p<<"\t";
-    }
 
-
-    auto t3=l1.begin();
-    advance(t3, 1);
-    *t3 = 900;
-    cout<<"\nafter update\n";
-    for(auto p:l1){
-        cout<<p<<"\t";
-    }
-    auto g = find(l1.begin(), l1.end(), 900);
-    if(g!=l1.end()){
-        cout<<"found="<< *g;
-    }
-    else{
-        cout<<"not found";
+    // Display the list
+    cout << "Initial list: \n";
+    for(auto p : l1){
+        cout << p << "\t";
     }
     
-
+    // Uncomment below lines if you want to remove front and back elements
+    // l1.pop_front(); // Remove first element
+    // l1.pop_back(); // Remove last element
+    
+    cout << "\nAfter delete (if pop_front/pop_back used):\n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Insert an element at a specific position
+    auto t = l1.begin(); // Iterator pointing to the first element
+    advance(t, 2); // Move iterator to the 3rd position (0-based index)
+    l1.insert(t, 1000); // Insert 1000 at the 3rd position
+    
+    cout << "\nAfter inserting 1000 at position 3:\n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Remove an element at a specific position
+    auto t1 = l1.begin();
+    advance(t1, 3); // Move iterator to the 4th element (0-based index)
+    l1.erase(t1); // Remove the element at this position
+    
+    cout << "\nAfter removing element at position 4:\n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Update an element at a specific position
+    auto t3 = l1.begin();
+    advance(t3, 1); // Move iterator to the 2nd element (0-based index)
+    *t3 = 900; // Update the value at this position
+    
+    cout << "\nAfter updating the 2nd element to 900:\n";
+    for(auto p : l1){
+        cout << p << "\t";
+    }
+    
+    // Searching for an element in the list
+    auto g = find(l1.begin(), l1.end(), 900); // Find 900 in the list
+    if(g != l1.end()){
+        cout << "\nElement found: " << *g;
+    }
+    else{
+        cout << "\nElement not found";
+    }
+    
+    return 0;
 }
